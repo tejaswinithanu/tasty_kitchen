d2250ed7566691122529ae84a7c00d71e3c8906d
Jahnavi-Pennabadi <jahnavipennabadi2001@gmail.com>
2024-10-07 14:03:33 +0530

    backend folder added

---
+++ backend/nest-project/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { BooksModule } from './books/books.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UserModule } from './user/user.module';
+
+
+
+
+
+@Module({
+  imports: [SequelizeModule.forRoot({dialect : 'sqlite',
+    storage : "../../books.sqlite",
+    autoLoadModels :true,
+    synchronize : true
+}),BooksModule,UserModule],
+})
+export class AppModule {}
+ 
---
+++ backend/nest-project/src/user/user.controller.ts
@@ -0,0 +1,39 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  register(@Body() createUserDto: User) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Post('login')
+  login(@Body() CreateUserDto : User){
+    return this.userService.login(CreateUserDto)
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: User) {
+    return this.userService.update(+id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
+}
